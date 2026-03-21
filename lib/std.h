7647084cd6bd441454bec2cf8b2d60d2ccce6622
WyattMallon <wyattmallon@gmail.com>
2026-03-21 14:26:00 -0400

    standard lib (in theory)

---
+++ lib/std.h
@@ -0,0 +1,3 @@
+int strlen(char * str);
+int atoi(char * str);
+char * itoa(int n);
---
+++ src/std.c
@@ -0,0 +1,44 @@
+#include "std.h"
+
+int strlen(char * str) {
+	char * p = str;
+	while (*p != '\0') {
+		p++;
+	}
+	return p - str;
+}
+
+int atoi(char * str) {
+	int result = 0;
+	int sign = 1;
+	int i = 0;
+
+	if (str[i] == '-') {
+		sign = -1;
+		i++;
+	}
+
+	while (str[i] >= '0' && str[i] <= '9') {
+		result = result * 10 + (str[i] - '0');
+		i++;
+	}
+
+	return result * sign;
+}
+
+char * itoa(int n) {
+	int size = 0;
+	int temp = n;
+	while (temp > 10) {
+		temp /= 10;
+		size++;
+	}
+
+	static char str[size];
+	for (int i = size; i > 0; i--) {
+		str[i] = n % 10;
+		n = n / 10;
+	}
+
+	return str;
+}
