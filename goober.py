f34548d7fb36a673e0b843f774f6dd933b72974a
artificialair <jaybaker964@gmail.com>
2026-03-22 07:31:23 -0400

    weeeeeeeeeeeeeeeeeeee

--- goober.py
+++ goober.py
@@ -20,3 +20,27 @@ for i in range(depth):
     array.append(d);
 print()
 print(len(array), len(array[0]), len(array[0][0]))
+
+# two year old level tkinter python renderer
+import tkinter as tk
+
+width = 200
+height = 200
+
+def rgb_to_hex(r, g, b):
+    return '#{:02x}{:02x}{:02x}'.format(r, g, b)      
+
+root = tk.Tk()
+
+canvas = tk.Canvas(root, width=width, height=height, bg='#FFFFFF')
+canvas.pack()
+
+def draw_pixels():
+    for i in range(len(array)):
+        for j in range(len(array[i])):
+            c = array[i][j]
+            c = [int(aga * 256) for aga in c]
+            canvas.create_rectangle(i, j, i+1, j+1, outline=rgb_to_hex(c[0], c[1], c[2]))
+
+draw_pixels()
+root.mainloop()  # make it not close
