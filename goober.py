84525fe6915a0a1cb13ec455ec2cc20613493ee5
WyattMallon <wyattmallon@gmail.com>
2026-03-22 07:16:46 -0400

    load c in python

--- goober.py
+++ goober.py
@@ -5,14 +5,18 @@ lib = ctypes.CDLL("./tracer.so")
 depth, rows, cols = 200, 200, 3
 total_elements = depth * rows * cols
 
-lib.render.restype = ctypes.POINTER(ctypes.c_float * total_elements)
+lib.render.restype = ctypes.POINTER(ctypes.POINTER(ctypes.POINTER(ctypes.c_float)))
 
 ptr = lib.render()
 
-data_array = ptr.contents
-
-d, r, c = 1, 1, 1
-flat_index = (d * rows * cols) + (r * cols) + c
-value = data_array[flat_index]
-
-print(f"Value at 3D[{d}][{r}][{c}]: {float(value)}")
+array = []
+for i in range(depth):
+    d = []
+    for j in range(rows):
+        r = []
+        for k in range(cols):
+            r.append(ptr[i][j][k]);
+        d.append(r);
+    array.append(d);
+print()
+print(len(array), len(array[0]), len(array[0][0]))
